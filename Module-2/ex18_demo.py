33e15fa86b6393cd2beb30556cf9fbcda980eef0
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2024-11-06 08:35:38 +0700

    ex14 updated

---
+++ Module-2/ex18_demo.py
@@ -0,0 +1,14 @@
+def do_nothing():
+    pass
+
+def do_something():
+    print("I did something")
+
+def do_more_things(a,b):
+    print("A IS",a,"B IS",b)
+
+
+
+do_nothing()
+do_something()
+do_more_things("hello",1)
